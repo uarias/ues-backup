5965e12a07a728d3c4d02f40d080e3216bf7ab25
Ulises Arias <ulisesariasz@gmail.com>
2010-11-01 19:17:16 -0600

    Usar Config::Simple en lugar de funcion limpio

--- backup_site.pl
+++ backup_site.pl
@@ -1,43 +1,24 @@
 #!/usr/bin/env perl
-
-# por huevos tiene que venir esto
-$backup_folder = '/var/backup/sitio/';
+# fecha
 $fecha = `date +%Y%m%d`;
 
-#declaraciones
+# declaraciones
 use File::Basename;
-my $config_file = dirname($0) . "./backup.conf";
-my @todos_lugares = quitarComentarios(getContenidoArchi($config_file));
+use Config::Simple;
+
+# Archivo de configuración
+my $config_file = $opt_c || "./backup.conf";
+Config::Simple->import_from($config_file, \%bconf);
+my $backup_folder = "ORIG.web";
+my $backup_destino = "DEST.webbackup";
 
 chdir($backup_folder) or die ("No puedo entrar en el directorio '$backup_folder'");
 
-foreach my $lugares_directorio (@todos_lugares) {
-    my($carpeta, $lugar_backup) = split(/\s+/,$lugares_directorio);
 
-    print "Haciendo Backup de $carpeta en $lugar_backup...";
-    `rsync -a $carpeta $backup_folder/$lugar_backup`;
+    print "Haciendo Backup de $backup_folder en $backup_destino...";
+    `rsync -a  $backup_folder $backup_destino`;
     print "Comprimiendo..." ;
     `tar -jcvf $fecha.tar.bz2 $lugar_backup`;
     print "Listo\n";
-}
 
-sub getContenidoArchi {
-    my $archivo = shift;
-    my @lineas;
-    if(!open(FILE,$archivo)){
-	die("No puedo abrir '$archivo': $!");
-    }else{
-	@lineas=<FILE>;
-	close(FILE);
-    }
-    return @lineas;
-}
-
-sub quitarComentarios {
-    my @lineas = @_;
-
-    @limpio = grep(!/^\s*#/, @lineas); #quito comentarios
-    @limpio = grep(!/^\s*$/, @limpio); #quito lineas vacías
-    
-    return @limpio;
 }
